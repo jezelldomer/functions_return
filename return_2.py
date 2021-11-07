5868dcbd06a6164b9bfa70a63e75db5e9dd4cfd1
Jezell <jzlldomer@gmail.com>
2021-11-07 12:08:07 +0800

    Added Features

--- return_2.py
+++ return_2.py
@@ -1,10 +1,12 @@
 import math
 
 def user_price():
-    _apple = int(input("How many apples you want to buy: "))
-    _orange = int(input("How many oranges you want to buy: "))
+    _apple = int(input("\033[0;35;40m \n How many apples you want to buy:  "))
+    _orange = int(input("\033[0;36;40m \n How many oranges you want to buy:  "))
     _grandtotal = obtain_totalPrice(appleF = _apple, orangeF = _orange)
-    print(f"The total amount is {_grandtotal:,.2f}")
+    print(f"\033[1;32;40m \n The total amount is \033[5;33;40m {_grandtotal:,.2f} ")
+
+#Added Python Color Class
 
 def obtain_totalPrice(appleF, orangeF):
     total = (20 * appleF) + (25 * orangeF)
