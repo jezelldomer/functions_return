4637abf1bab6566fd23e1ff0b3957713c54282cc
Jezell <jzlldomer@gmail.com>
2021-11-07 12:33:55 +0800

    second commit

--- return_2.py
+++ return_2.py
@@ -1,1 +1,13 @@
-def 
+import math
+
+def user_price():
+    _apple = int(input("How many apples you want to buy: "))
+    _orange = int(input("How many oranges you want to buy: "))
+    _grandtotal = obtain_totalPrice(appleF = _apple, orangeF = _orange)
+    print(f"The total amount is {_grandtotal:,.2f}")
+
+def obtain_totalPrice(appleF, orangeF):
+    total = (20 * appleF) + (25 * orangeF)
+    return total
+
+user_price()
