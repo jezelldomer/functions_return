5b9dd799e411bc66339a06bff775949244a17902
Jezell <jzlldomer@gmail.com>
2021-11-07 11:58:32 +0800

    third commit

--- return_1.py
+++ return_1.py
@@ -5,3 +5,9 @@ def name_age_address():
     _address = input("Enter your address: ")
     return _name, _age, _address
 
+def display(nameG, ageG, addG): 
+    print(f"Hi, my name is {nameG}, I am {ageG} years old and I live in {addG}. ")
+
+name, age, address = name_age_address()
+
+display (name, age, address)
