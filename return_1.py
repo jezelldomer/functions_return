9d6faf5fd32fd9d0a8b429076730c214124ee0b8
Jezell <jzlldomer@gmail.com>
2021-11-07 11:52:38 +0800

    second commit

--- return_1.py
+++ return_1.py
@@ -1,1 +1,7 @@
-def 
+
+def name_age_address():
+    _name = input("Enter your name: ")
+    _age = int(input("Enter your age: "))
+    _address = input("Enter your address: ")
+    return _name, _age, _address
+
