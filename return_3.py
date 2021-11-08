86d4c26a81ea3685a1d26a9ff56236bb37746bdc
Jezell <jzlldomer@gmail.com>
2021-11-08 18:36:15 +0800

    second commit

--- return_3.py
+++ return_3.py
@@ -3,4 +3,7 @@ import math
 def info():
     _money = float(input("Enter your money: "))
     _apple = float(input("What is the cost of an apple?: "))
-
+    total =  int(_money / _apple)
+    result = (total * _apple)
+    grand_total = (_money - result)
+    return total, grand_total
